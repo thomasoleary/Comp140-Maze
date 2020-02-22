345f0a17ea15b8c4e13fa87446f81c985da7b1e7
Thomas O'Leary <TO231922@falmouth.ac.uk>
2020-02-22 23:33:38 +0000

    Created Arduino.cs, so that the arduino is able to connect to Unity. Created empty script BallTrigger.cs

---
+++ Assets/Arduino.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System;
+using System.IO.Ports;
+
+public class Arduino : MonoBehaviour
+{
+
+    [SerializeField]
+    private int commPort = 0;
+
+    private SerialPort serial = null;
+
+    void Start()
+    {
+        ConnectToSerial();
+    }
+
+    void ConnectToSerial()
+    {
+        Debug.Log("Attempting Serial: " + commPort);
+
+        serial = new SerialPort("\\\\.\\COM" + commPort, 9600);
+        serial.ReadTimeout = 50;
+        serial.Open();
+    }
+
+    void WriteToArduino(string motor)
+    {
+        serial.WriteLine(motor);
+        serial.BaseStream.Flush();
+    }
+
+    void OnDestroy()
+    {
+        Debug.Log("Exiting");
+        serial.Close();
+    }
+}
