57c38b4182af7cf74fde80ea99bffb3072ca6965
Thomas O'Leary <to231922@falmouth.ac.uk>
2020-02-24 12:23:27 +0000

    Changed OnTriggerEnter & OnTriggerExit in BallTrigger.cs to private. Added comments to BallTrigger.cs so that it explains the code within.

--- Maze-Game/Assets/BallTrigger.cs
+++ Maze-Game/Assets/BallTrigger.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class BallTrigger : MonoBehaviour
 {
+    // This is a script that applies to all triggers
+    // that are children of the Ball object
+
+    // Creating a public bool that will be accessed in BallScript.cs
+    // This bool will vary on each trigger
+    // Allowing the arduino to activate the different motors
+
+    // Automatically set to false (otherwise the motor will turn on instantly)
     public bool ballTriggered = false;
 
-    void OnTriggerEnter(Collider other)
+
+
+    // When the wall enters the trigger
+    // Bool is set to true
+    private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "wall")
         {
             ballTriggered = true;
         }
     }
-    void OnTriggerExit(Collider other)
+
+    // When the wall leaves the trigger
+    // Bool is set back to false
+    private void OnTriggerExit(Collider other)
     {
         if (other.tag == "wall")
         {
